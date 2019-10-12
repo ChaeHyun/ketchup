43776c73dd8ddad5fcaeb9b35eedbee3b84ee136
CH <iamrch9@gmail.com>
2019-10-12 15:23:23 +0900

    AddEditTaskViewModel은 AddEditTaskFragment에서 사용할 데이터를 홀드하는 역할을 맡는다. AddEditTaskFragment는 하나의 Task를 생성,수정,삭제하는 기능을 수행한다.
    LiveData<Task>를 가지고 TaskRepository에게 데이터조작을 요청한다.

--- app/src/main/java/com/ketchup/AddEditTaskViewModel.java
+++ app/src/main/java/com/ketchup/AddEditTaskViewModel.java
@@ -1,17 +1,64 @@
 package com.ketchup;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.ketchup.model.task.Task;
 import com.ketchup.model.task.TaskRepository;
 
+import java.util.UUID;
+import java.util.concurrent.Executors;
+
 import javax.inject.Inject;
+
+import timber.log.Timber;
 
 public class AddEditTaskViewModel extends ViewModel {
 
     private TaskRepository taskRepository;
 
+    private MutableLiveData<Task> _task = new MutableLiveData<>();
+    private LiveData<Task> task = _task;
+
     @Inject
     public AddEditTaskViewModel(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
+    }
+
+    public LiveData<Task> getTask() {
+        return task;
+    }
+
+    // EDIT MODE 시에 기존 taskId를 전달받았을 경우 읽어오기
+    public void loadTaskByUuid(final String uuid) {
+        Executors.newSingleThreadExecutor().execute(() ->
+                _task.postValue(taskRepository.getTask(UUID.fromString(uuid)))
+        );
+    }
+
+
+    public void updateTask(final Task task) {
+        Timber.i("[ insertTask ] : repo.updateTask(Task)");
+        Executors.newSingleThreadExecutor().execute(() ->
+                taskRepository.updateTask(task)
+        );
+    }
+
+    public void insertTask(Task task) {
+        if (task == null)
+            return;
+        Executors.newSingleThreadExecutor().execute(() -> {
+            Timber.i("[ insertTask ] : repo.insertTask(Task)");
+            taskRepository.insertTask(task);
+            _task.postValue(task);
+        });
+
+    }
+
+    public void deleteTask(final String uuid) {
+        Executors.newSingleThreadExecutor().execute(() ->
+                taskRepository.deleteTask(UUID.fromString(uuid))
+        );
     }
 }
